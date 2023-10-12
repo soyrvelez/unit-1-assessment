6540212e7290da7abafe5fa99f2d7af2893d3432
Rodrigo <velez59@gmail.com>
2023-10-11 19:33:27 -0700

    feat: added alphabet navigator

--- js/main.js
+++ js/main.js
@@ -1,18 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
   const plusButton = document.querySelector('#plus');
   const minusButton = document.querySelector('#minus');
+  const plusAlphaButton = document.querySelector('#plus-alpha');
+  const minusAlphaButton = document.querySelector('#minus-alpha');
   const userInput = document.querySelector('#user-input');
+  const userInputAlpha = document.querySelector('#user-input-alpha');
   const countField = document.querySelector('#count');
+  const alphaField = document.querySelector('#current-alpha');
+  const alphabet = ["a","b","c","d","e","f","g","h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
   plusButton.addEventListener('click', function (e) {
     countField.textContent = Number(countField.textContent) + Number(userInput.value);
-    updateColor()
+    updateColor();
   });
 
   minusButton.addEventListener('click', function (e) {
     countField.textContent = Number(countField.textContent) - Number(userInput.value);
-    updateColor()
+    updateColor();
   });
+
+  plusAlphaButton.addEventListener('click', function (e) {
+    alphaField.textContent = alphabet[alphabet.indexOf(alphaField.textContent.toLowerCase()) + Number(userInputAlpha.value)].toUpperCase();
+  });
+
+  minusAlphaButton.addEventListener('click', function (e) {
+    alphaField.textContent = alphabet[alphabet.indexOf(alphaField.textContent.toLowerCase()) - Number(userInputAlpha.value)].toUpperCase();
+  });
+
 
   function updateColor(){
     if (Number(countField.textContent) < 0) {
